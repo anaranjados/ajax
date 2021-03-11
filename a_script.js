782c24d48b077833979a580f8d9f027a7bd3f72c
Alejandro Proenza <dev@laraceramica.com>
2021-03-11 12:41:16 +0300

    Update a_script.js
    replaced with arrow functions as EcmaScript-6

--- a_script.js
+++ a_script.js
@@ -1,20 +1,17 @@
-const innNorma  = 12;
-const accNorma	= 20;
-const bikNorma	= 9;
-const listOfCust= document.querySelector('select');
-let allCol	    = document.querySelectorAll('.account');
+const innNorma  = 12, accNorma	= 20, bikNorma	= 9;
 const emptyCol  = allCol[0];
+const listOfCust= document.querySelector('select');
+let allCol	= document.querySelectorAll('.account');
 //
-let idObj		= document.querySelector('input[name=id]');
-let custNameObj = document.querySelector('input[name=cust_name]');
-let innObj 		= document.querySelector('input[name=inn]');
-let addressObj  = document.querySelector('input[name=address]');
+let idObj	= document.getElementsByName('id');
+let custNameObj = document.getElementsByName('cust_name');
+let innObj 	= document.getElementsByName('inn');
+let addressObj  = document.getElementsByName('address');
 let accNumber 	= document.getElementsByName('acc_number');
 let accName 	= document.getElementsByName('acc_name');
-let bik 		= document.getElementsByName('bik');
+let bik 	= document.getElementsByName('bik');
 let balance 	= document.getElementsByName('balance');
 //
-
 
 // -- Getting list of customers
 window.addEventListener('load', getList);
@@ -22,7 +19,7 @@ window.addEventListener('load', getList);
 function getList() {
 	const send = { field: "cust_name", table: "customer" };
 	const add = document.createElement("script");
-	add.src = "get_list.php?params=" + JSON.stringify(send);
+	add.src = "php/get_list.php?params=" + JSON.stringify(send);
 	document.body.appendChild(add);
 }
 
@@ -37,7 +34,7 @@ function getList_sub(dataset) {
 
 
 // ---- Filling the fields ----
-listOfCust.addEventListener('change', function() {
+listOfCust.addEventListener('change', ()=> {
 	allCol= document.querySelectorAll('.account');
 	for (i in allCol) {
 		try {	allCol[i].remove() }
@@ -45,7 +42,7 @@ listOfCust.addEventListener('change', function() {
 	}
 	const send = { name: this.value };
 	const add = document.createElement("script");
-	add.src = "filling.php?params=" + JSON.stringify(send);	// result appears after 6 functions above
+	add.src = "php/filling.php?params=" + JSON.stringify(send);	// result appears after 6 functions above
 	document.body.appendChild(add);
 });
 
@@ -69,7 +66,7 @@ function filling_sub(dataset) {
 	}
 	else if (dataset.length > 0) {
 		for (i in dataset) {
-			sect.innerHTML 	  += emptyCol.outerHTML;		// as rows come, so columns are created
+			sect.innerHTML 	  += emptyCol.outerHTML;		// as rows come, so many columns are created
 		}
 		let custId = [];
 		const butt_acc	= document.querySelectorAll('.account .buttons');
@@ -104,7 +101,7 @@ function filling_sub(dataset) {
 		} } catch { console.log('Miscounted buttons quantity'); }
 		// Adding Event Listener for new account buttons
 		for (i in butt_acc) {		
-			butt_acc[i].onmousedown = function(e) {
+			butt_acc[i].onmousedown = (e)=> {
 				const str = e.target.parentNode.className;
 				const type = e.target.className;
 				const colNum = parseInt(str.substring(str.length-2, str.length));
@@ -129,7 +126,7 @@ function account(t, c, idObj, aNu, aNa, bi, bal) {
 		// if ( !isNaN(aNu)  &&  !isNaN(bik)  &&  !isNaN(balance) ) {	// toString(aNu).length === accNorma   &&  toString(bik).length === bikNorma 
 			const send = { id: idObj, acc_number: aNu, acc_name: aNa, bik: bi, balance: bal };
 			const add = document.createElement("script");
-			add.src = 'btn_save_acc.php?params=' +JSON.stringify(send);
+			add.src = 'php/btn_save_acc.php?params=' +JSON.stringify(send);
 			document.body.appendChild(add);
 		// }	else {alert('Please fill Account correctly')}
 	}
@@ -137,7 +134,7 @@ function account(t, c, idObj, aNu, aNa, bi, bal) {
 		// if ( !isNaN(aNu)  &&  !isNaN(bik)  &&  !isNaN(balance) ) {	// toString(aNu).length === accNorma   &&  toString(bik).length === bikNorma 
 			const send = { id: idObj, acc_number: aNu, acc_name: aNa, bik: bi, balance: bal };
 			const add = document.createElement("script");
-			add.src = 'btn_add_acc.php?params=' +JSON.stringify(send);
+			add.src = 'php/btn_add_acc.php?params=' +JSON.stringify(send);
 			document.body.appendChild(add);
 		//}		else {alert('Please fill Account correctly')}
 	}
@@ -145,11 +142,11 @@ function account(t, c, idObj, aNu, aNa, bi, bal) {
 		//if ( toString(aNu).length === accNorma  &&  !isNaN(aNu) ) {
 			const send = { acc_number: aNu };
 			const add = document.createElement("script");
-			add.src = 'btn_del_acc.php?params=' +JSON.stringify(send);
+			add.src = 'php/btn_del_acc.php?params=' +JSON.stringify(send);
 			document.body.appendChild(add);
 		// }	else {alert('This account does not exist, please enter 20 digits')}
 	}
-	else { console.log('No buttons were heard.')}
+	else { console.log('No button was clicked.')}
 	getList();
 }
 
@@ -164,7 +161,7 @@ function updateActC() {
 		const send = { name: custNameObj.value, inn: innObj.value, address: addressObj.value, id: idObj.value };
 		try {
 			const add = document.createElement("script");
-			add.src = "btn_save_cust.php?params=" + JSON.stringify(send);
+			add.src = "php/btn_save_cust.php?params=" + JSON.stringify(send);
 			document.body.appendChild(add);
 			}
 		catch { console.log('Got issues in request SAVE (cust)') }
@@ -180,7 +177,7 @@ function addActC() {
 	if (custNameObj.value != "" && !isNaN(parseInt(innObj.value)) && innObj.value.length === innNorma) {
 		const send = { name: custNameObj.value, inn: innObj.value, address: addressObj.value };
 		try {
-			add.src = "btn_new_cust.php?params=" + JSON.stringify(send);
+			add.src = "php/btn_new_cust.php?params=" + JSON.stringify(send);
 			document.body.appendChild(add);
 			}
 		catch { console.log('Got issues in request NEW (cust)') }
@@ -193,7 +190,7 @@ function delActC() {
 	const send = { id: idObj.value };
 	try {
 		// let add = document.createElement("script");
-		add.src = "btn_del_cust.php?params=" + JSON.stringify(send);
+		add.src = "php/btn_del_cust.php?params=" + JSON.stringify(send);
 		document.body.appendChild(add);
 		}
 	catch { console.log('Got issues in request DEL (cust)') }
