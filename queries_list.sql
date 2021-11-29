4baf63c929326975e200937580e7969f87aaf36e
Juan Espinoza <dev@laraceramica.com>
2021-11-29 17:15:19 +0300

    Update queries_list.sql
    excepted own names

--- queries_list.sql
+++ queries_list.sql
@@ -11,7 +11,7 @@ create table Customer(
 create table Account(
 			cust_id tinyint(255) NOT NULL,
 			acc_number decimal(20,0),
-      acc_name varchar(100) CHARSET cp1251 COLLATE cp1251_general_ci,
+      			acc_name varchar(100) CHARSET cp1251 COLLATE cp1251_general_ci,
 			bik decimal(9,0) NOT NULL,
 			balance decimal(14,2) NOT NULL,
 			PRIMARY KEY (acc_number), INDEX(cust_id)
@@ -25,17 +25,17 @@ FLUSH PRIVILEGES;
 
 
 insert into Customer (cust_name, inn, address) VALUES
-                        ('Vasilisa', 323, 'Tsarstvo');
+                        ('Agustín', 321, 'Canarias');
 insert into Customer (cust_name, inn, address) VALUES
-                        ('Innokentiy', 123456789, 'Lives in AZ');
+                        ('Jacinto', 135790, 'Las Palmas');
 insert into Customer (cust_name, inn, address) VALUES
-                        ('Guzman', 523456789, 'CU');
+                        ('Maceo ', 024689, 'CU');
 
 insert into Account (cust_id, acc_number, acc_name, bik, balance) VALUES
-                        (1, 323, 'Noviy Urengoi', 246, 354548945.12);
+                        (1, 323, 'Kunashir', 07016446, 354548945.12);
 insert into Account (cust_id, acc_number, acc_name, bik, balance) VALUES
-                        (2, 07645637, 'Kesha in Africa', 108568346, 8945.34);
+                        (2, 07645637, 'Galápogos', 78568345, 8945.34);
 insert into Account (cust_id, acc_number, acc_name, bik, balance) VALUES
-                        (2, 8645637, 'Somebody calling K', 108568346, 8945.34);
+                        (2, 8645637, 'Cauto', 10858346, 8945.34);
 insert into Account (cust_id, acc_number, acc_name, bik, balance) VALUES
-                        (3, 33546347234465754, 'Banco nacional', 46, 6548945.12);
+                        (3, 33546347234465754, 'Jatibonico', 6001159, 6548945.12);
